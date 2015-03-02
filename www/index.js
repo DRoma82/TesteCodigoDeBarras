cb28f45d8be8602bfe7d516fbe364a019e7c8403
DRoma82 <daniel.roma@gmail.com>
2015-03-02 14:26:04 -0300

    ...

--- www/index.js
+++ www/index.js
@@ -4,5 +4,15 @@ $(function () {
 
 function fnLer()
 {
-    navigator.notification.vibrate(500);
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
 }
