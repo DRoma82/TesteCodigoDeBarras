681dff1708cdbedabbe1039d3571d530d4a1a8f8
DRoma82 <daniel.roma@gmail.com>
2015-03-02 14:38:32 -0300

    ...

--- www/index.js
+++ www/index.js
@@ -4,15 +4,14 @@ $(function () {
 
 function fnLer()
 {
-    cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      }
-   );
+    var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+    scanner.scan(function (result) {
+        alert("We got a barcode\n" +
+        "Result: " + result.text + "\n" +
+        "Format: " + result.format + "\n" +
+        "Cancelled: " + result.cancelled);
+    }, function (error) {
+        alert("Scanning failed: ", error);
+    });
 }
